975ddfaea3d71f2060442d1e41c73c15fd39ade4
Takahashi-Kaisei <130371620+Takahashi-Kaisei@users.noreply.github.com>
2026-05-18 06:52:10 +0000

    fix: uv の python version を 3.10.* に緩める。あとは微調整

--- MoE-LLaVA/setup_moellava.sh
+++ MoE-LLaVA/setup_moellava.sh
@@ -8,4 +8,7 @@ uv pip install -e ".[train]"
 uv pip install ../DeepSpeed-0.9.5/
 
 uv pip install "setuptools<82"
+
 uv pip install "flash-attn==2.3.5" --no-build-isolation
+
+uv tool install huggingface_hub
